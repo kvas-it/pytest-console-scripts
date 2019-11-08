b5650c58c5e8b81ff378ca65a13d215747227b2f
Vasily Kuznetsov <kvas.it@gmail.com>
2019-11-08 19:35:38 +0100

    Release version 0.2.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name='pytest-console-scripts',
-    version='0.1.10',
+    version='0.2.0',
     author='Vasily Kuznetsov',
     author_email='kvas.it@gmail.com',
     maintainer='Vasily Kuznetsov',
